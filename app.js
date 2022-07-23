45f8774a52050c0b748f8e989739b910dacc3391
kenkuo86 <kenkuo860129@gmail.com>
2022-07-23 13:07:19 +0800

    Add search page - showing searched restaurants

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ app.get('/restaurants/:id', (req, res) => {
   res.render('show', { restaurant: restaurantInfo })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const noResultMessage = '查無資料，請更換關鍵字或回到首頁'
+
+  const searchedRestaurants = restaurantsInfos.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+
+  // 根據搜尋結果調整頁面資訊
+  searchedRestaurants.length ? res.render('index', { restaurant: searchedRestaurants, keyword: keyword }) : res.render('index', { noResultMessage: noResultMessage, keyword: keyword })
+})
+
 app.listen(port, () => {
   console.log(`server listen on http://localhost:${port}`)
 })
