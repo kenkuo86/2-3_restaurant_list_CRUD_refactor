5fea8513a712fa2746004d9d29741c9ee24df118
kenkuo86 <kenkuo860129@gmail.com>
2022-08-03 02:04:18 +0800

    feat: edit restaurant info complete

--- app.js
+++ app.js
@@ -107,6 +107,38 @@ app.get('/restaurants/edit/:id', (req,res) => {
     .catch( error => console.log(error))
 })
 
+app.post('/restaurant/edit/:id', (req,res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+          .then( restaurant => {
+            restaurant.name = name
+            restaurant.name_en = name_en
+            restaurant.category = category
+            restaurant.image = image
+            restaurant.location = location
+            restaurant.phone = phone
+            restaurant.google_map = google_map
+            restaurant.rating = rating
+            restaurant.description = description
+
+            return restaurant.save()
+          })
+          .then( () => { res.redirect(`/restaurants/${id}`)} )
+          .catch( error => console.log(error) )
+})
+
+
+
 // 監聽
 app.listen(port, () => {
   console.log(`server listen on http://localhost:${port}`)
